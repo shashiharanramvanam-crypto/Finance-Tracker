d8ba6924a2321fb72e80198a79e2e88daae4fd97
Shashiharan <shashiharanramvanam@gmail.com>
2026-09-02 20:36:19 +0530

    Changed amount type to decimal and float for business logic in services

--- src/models.py
+++ src/models.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass,field
 import uuid
+from decimal import Decimal
 
 @dataclass
 class Transaction:
     id:str = field(default_factory=lambda: str(uuid.uuid4()))
-    amount:float=0.0
+    amount:float | Decimal =0.0
     category:str=""
     date:str=""
     type:str=""
